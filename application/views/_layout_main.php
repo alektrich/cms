46d39448fad54bd50d157ecac6993370721c1c78
Aleksandar Trickovic <alektrich@gmail.com>
2013-01-31 22:35:08 +0100

    CMS / first commit

---
+++ application/views/_layout_main.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $meta_title; ?></title>
+	<!-- Bootstrap-->
+	<link href="" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="navbar navbar-static-top navbar-inverse">
+    	<div class="navbar-inner">
+    		<a class="brand" href="#">Title</a>
+    		<ul class="nav">
+    			<li class="active"><a href="#">Home</a></li>
+    			<li><a href="#">Link</a></li>
+    			<li><a href="#">Link</a></li>
+    		</ul>
+    	</div>
+    </div>
+	<h1>Hello, world!</h1>
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+	<script src="<?php echo site_url('js/bootstrap.min.js'); ?>"></script>
+</body>
+</html>
