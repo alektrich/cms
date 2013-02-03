babbeb5df1b6364dfd6a581ee6e69e352def95f2
Aleksandar Trickovic <alektrich@gmail.com>
2013-02-03 19:19:02 +0100

    baza again

--- cms.sql
+++ cms.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 01, 2013 at 08:05 PM
+-- Generation Time: Feb 03, 2013 at 07:18 PM
 -- Server version: 5.5.25a
 -- PHP Version: 5.4.4
 
@@ -23,6 +23,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `ci_sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+  `session_id` varchar(40) NOT NULL DEFAULT '0',
+  `ip_address` varchar(45) NOT NULL DEFAULT '0',
+  `user_agent` varchar(120) NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_data` text NOT NULL,
+  PRIMARY KEY (`session_id`),
+  KEY `last_activity_idx` (`last_activity`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `ci_sessions`
+--
+
+INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
+('3baee9fe7ed5acb8da8612667fcec960', '127.0.0.1', 'Mozilla/5.0 (Windows NT 5.1; rv:18.0) Gecko/20100101 Firefox/18.0', 1359913222, 'a:5:{s:9:"user_data";s:0:"";s:4:"name";s:10:"Aleksandar";s:5:"email";s:19:"alektrich@gmail.com";s:2:"id";s:1:"1";s:8:"loggedin";b:1;}');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -35,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `migrations` (
 --
 
 INSERT INTO `migrations` (`version`) VALUES
-(2);
+(3);
 
 -- --------------------------------------------------------
 
@@ -64,7 +87,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(128) NOT NULL,
   `name` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `name`) VALUES
+(1, 'alektrich@gmail.com', '0ad6d3aafb3aae99bab654e6421215a792d6077145771c2561b040212f606a4a352e66f730c76ab9ebd62f0429fdf9af5aef1f2829ae62ad67c07eea6b6c4b41', 'Aleksandar');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
